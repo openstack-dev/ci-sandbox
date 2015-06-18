646acc01ab489c7461d83fc4c5e07c88e7d1d538
Michael Johnson <johnsomor@gmail.com>
2015-06-18 17:47:41 +0000

    Just a test file - Do not merge
    Change-Id: Ie40476439ab1e56a9fc02e1516417f1cf58b3248

---
+++ octavia/amphorae/drivers/haproxy/jinja/jinja_cfg.py
@@ -0,0 +1,268 @@
+#    Copyright (c) 2015 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+import jinja2
+import six
+
+from octavia.common import constants
+
+PROTOCOL_MAP = {
+    constants.PROTOCOL_TCP: 'tcp',
+    constants.PROTOCOL_HTTP: 'http',
+    constants.PROTOCOL_HTTPS: 'tcp',
+    constants.PROTOCOL_TERMINATED_HTTPS: 'http'
+}
+
+BALANCE_MAP = {
+    constants.LB_ALGORITHM_ROUND_ROBIN: 'roundrobin',
+    constants.LB_ALGORITHM_LEAST_CONNECTIONS: 'leastconn',
+    constants.LB_ALGORITHM_SOURCE_IP: 'source'
+}
+
+ACTIVE_PENDING_STATUSES = constants.SUPPORTED_PROVISIONING_STATUSES + (
+    constants.DEGRADED,)
+
+BASE_PATH = '/var/lib/octavia'
+BASE_CRT_DIR = '/listeners'
+
+HAPROXY_TEMPLATE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__),
+                 'templates/haproxy_listener.template'))
+
+JINJA_ENV = None
+
+
+class JinjaTemplater(object):
+
+    def __init__(self,
+                 base_amp_path=None,
+                 base_crt_dir=None,
+                 haproxy_template=None,
+                 log_http=None,
+                 log_server=None,
+                 timeout_client=None,
+                 timeout_server=None,
+                 timeout_connect=None):
+        """HaProxy configuration generation
+
+        :param base_amp_path: Base path for amphora data
+        :param base_crt_dir: Base directory for certificate storage
+        :param haproxy_template: Absolute path to Jinja template
+        :param log_http: Haproxy HTTP logging path
+        :param log_server: Haproxy Server logging path
+        :param timeout_client: Timeout client
+        :param timeout_server: Timeout server
+        :param timeout_connect: Timeout connect
+        """
+
+        self.base_amp_path = base_amp_path if base_amp_path else BASE_PATH
+        self.base_crt_dir = base_crt_dir if base_crt_dir else BASE_CRT_DIR
+        self.haproxy_template = (haproxy_template if haproxy_template
+                                 else HAPROXY_TEMPLATE)
+        self.cert_store_path = '{0}{1}'.format(self.base_amp_path,
+                                               self.base_crt_dir)
+        self.log_http = log_http
+        self.log_server = log_server
+        self.timeout_client = timeout_client
+        self.timeout_server = timeout_server
+        self.timeout_connect = timeout_connect
+
+    def build_config(self, listener, tls_cert,
+                     socket_path=None,
+                     user_group='nogroup'):
+        """Convert a logical configuration to the HAProxy version
+
+        :param listener: The listener configuration
+        :param tls_cert: The TLS certificates for the listener
+        :param socket_path: The socket path for Haproxy process
+        :param user_group: The user group
+        :return: Rendered configuration
+        """
+        return self.render_loadbalancer_obj(listener,
+                                            tls_cert=tls_cert,
+                                            user_group=user_group,
+                                            socket_path=socket_path)
+
+    def _get_template(self):
+        """Returns the specified Jinja configuration template."""
+        global JINJA_ENV
+        if not JINJA_ENV:
+            template_loader = jinja2.FileSystemLoader(
+                searchpath=os.path.dirname(self.haproxy_template))
+            JINJA_ENV = jinja2.Environment(
+                loader=template_loader,
+                trim_blocks=True,
+                lstrip_blocks=True)
+        return JINJA_ENV.get_template(os.path.basename(self.haproxy_template))
+
+    def render_loadbalancer_obj(self, listener,
+                                tls_cert=None,
+                                user_group='nogroup',
+                                socket_path=None):
+        """Renders a templated configuration from a load balancer object
+
+        :param listener: The listener configuration
+        :param tls_cert: The TLS certificates for the listener
+        :param socket_path: The socket path for Haproxy process
+        :param user_group: The user group
+        :return: Rendered configuration
+        """
+        loadbalancer = self._transform_loadbalancer(
+            listener.load_balancer,
+            listener,
+            tls_cert)
+        if not socket_path:
+            socket_path = '%s/%s.sock' % (self.base_amp_path, listener.id)
+        return self._get_template().render(
+            {'loadbalancer': loadbalancer,
+             'user_group': user_group,
+             'stats_sock': socket_path,
+             'log_http': self.log_http,
+             'log_server': self.log_server,
+             'timeout_client': self.timeout_client,
+             'timeout_server': self.timeout_server,
+             'timeout_connect': self.timeout_connect},
+            constants=constants)
+
+    def _transform_loadbalancer(self, loadbalancer, listener, tls_cert):
+        """Transforms a load balanacer into an object that will
+
+           be processed by the templating system
+        """
+        listener = self._transform_listener(listener, tls_cert)
+        return {
+            'name': loadbalancer.name,
+            'vip_address': loadbalancer.vip.ip_address,
+            'listener': listener
+        }
+
+    def _transform_listener(self, listener, tls_cert):
+        """Transforms a listener into an object that will
+
+            be processed by the templating system
+        """
+        ret_value = {
+            'id': listener.id,
+            'protocol_port': listener.protocol_port,
+            'protocol_mode': PROTOCOL_MAP[listener.protocol],
+            'protocol': listener.protocol
+        }
+        if listener.connection_limit and listener.connection_limit > -1:
+            ret_value['connection_limit'] = listener.connection_limit
+        if listener.tls_certificate_id:
+            ret_value['default_tls_path'] = '%s/%s/%s.pem' % (
+                self.cert_store_path, listener.id, tls_cert.primary_cn)
+        if listener.sni_containers:
+            ret_value['crt_dir'] = '%s/%s' % (
+                self.cert_store_path, listener.id)
+        if listener.default_pool:
+            ret_value['default_pool'] = self._transform_pool(
+                listener.default_pool)
+        return ret_value
+
+    def _transform_pool(self, pool):
+        """Transforms a pool into an object that will
+
+            be processed by the templating system
+        """
+        ret_value = {
+            'id': pool.id,
+            'protocol': PROTOCOL_MAP[pool.protocol],
+            'lb_algorithm': BALANCE_MAP.get(pool.lb_algorithm, 'roundrobin'),
+            'members': [],
+            'health_monitor': '',
+            'session_persistence': '',
+            'enabled': pool.enabled,
+            'operating_status': pool.operating_status
+        }
+        members = [self._transform_member(x) for x in pool.members]
+        ret_value['members'] = members
+        if pool.health_monitor:
+            ret_value['health_monitor'] = self._transform_health_monitor(
+                pool.health_monitor)
+        if pool.session_persistence:
+            ret_value[
+                'session_persistence'] = self._transform_session_persistence(
+                pool.session_persistence)
+        return ret_value
+
+    @staticmethod
+    def _transform_session_persistence(persistence):
+        """Transforms session persistence into an object that will
+
+            be processed by the templating system
+        """
+        return {
+            'type': persistence.type,
+            'cookie_name': persistence.cookie_name
+        }
+
+    @staticmethod
+    def _transform_member(member):
+        """Transforms a member into an object that will
+
+            be processed by the templating system
+        """
+        return {
+            'id': member.id,
+            'address': member.ip_address,
+            'protocol_port': member.protocol_port,
+            'weight': member.weight,
+            'enabled': member.enabled,
+            'subnet_id': member.subnet_id,
+            'operating_status': member.operating_status
+        }
+
+    def _transform_health_monitor(self, monitor):
+        """Transforms a health monitor into an object that will
+
+            be processed by the templating system
+        """
+        return {
+            'id': monitor.id,
+            'type': monitor.type,
+            'delay': monitor.delay,
+            'timeout': monitor.timeout,
+            'fall_threshold': monitor.fall_threshold,
+            'rise_threshold': monitor.rise_threshold,
+            'http_method': monitor.http_method,
+            'url_path': monitor.url_path,
+            'expected_codes': '|'.join(
+                self._expand_expected_codes(monitor.expected_codes)),
+            'enabled': monitor.enabled,
+        }
+
+    @staticmethod
+    def _expand_expected_codes(codes):
+        """Expand the expected code string in set of codes.
+
+        200-204 -> 200, 201, 202, 204
+        200, 203 -> 200, 203
+        """
+
+        retval = set()
+        for code in codes.replace(',', ' ').split(' '):
+            code = code.strip()
+
+            if not code:
+                continue
+            elif '-' in code:
+                low, hi = code.split('-')[:2]
+                retval.update(
+                    str(i) for i in six.moves.xrange(int(low), int(hi) + 1))
+            else:
+                retval.add(code)
+        return retval
---
+++ octavia/tests/unit/certificates/generator/test_local.py
@@ -0,0 +1,181 @@
+# Copyright 2014 Rackspace US, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import datetime
+
+from OpenSSL import crypto
+import six
+
+import octavia.certificates.generator.local as local_cert_gen
+import octavia.tests.unit.base as base
+
+
+class TestLocalGenerator(base.TestCase):
+
+    def setUp(self):
+        self.signing_digest = "sha256"
+
+        # Set up CSR data
+        csr_key = crypto.PKey()
+        csr_key.generate_key(crypto.TYPE_RSA, 2048)
+        csr = crypto.X509Req()
+        csr.set_pubkey(csr_key)
+        self.certificate_signing_request = crypto.dump_certificate_request(
+            crypto.FILETYPE_PEM, csr
+        )
+
+        # Set up CA data
+        ca_key = crypto.PKey()
+        ca_key.generate_key(crypto.TYPE_RSA, 2048)
+
+        self.ca_private_key_passphrase = "Testing"
+        self.ca_private_key = crypto.dump_privatekey(
+            crypto.FILETYPE_PEM,
+            ca_key,
+            'aes-256-cbc',
+            self.ca_private_key_passphrase
+        )
+
+        ca_cert = crypto.X509()
+        ca_subject = ca_cert.get_subject()
+        ca_subject.C = "US"
+        ca_subject.ST = "Oregon"
+        ca_subject.L = "Springfield"
+        ca_subject.O = "Springfield Nuclear Power Plant"
+        ca_subject.OU = "Section 7-G"
+        ca_subject.CN = "maggie1"
+        ca_cert.set_issuer(ca_cert.get_subject())
+        ca_cert.set_pubkey(ca_key)
+        ca_cert.gmtime_adj_notBefore(0)
+        ca_cert.gmtime_adj_notAfter(2 * 365 * 24 * 60 * 60)
+        ca_cert.sign(ca_key, self.signing_digest)
+
+        self.ca_certificate = crypto.dump_certificate(
+            crypto.FILETYPE_PEM, ca_cert
+        )
+
+        super(TestLocalGenerator, self).setUp()
+
+    def test_sign_cert(self):
+        # Attempt sign a cert
+        signed_cert = local_cert_gen.LocalCertGenerator.sign_cert(
+            csr=self.certificate_signing_request,
+            validity=2 * 365 * 24 * 60 * 60,
+            ca_cert=self.ca_certificate,
+            ca_key=self.ca_private_key,
+            ca_key_pass=self.ca_private_key_passphrase,
+            ca_digest=self.signing_digest
+        )
+
+        self.assertIn("-----BEGIN CERTIFICATE-----", signed_cert)
+
+        # Load the cert for specific tests
+        cert = crypto.load_certificate(crypto.FILETYPE_PEM, signed_cert)
+
+        # Make sure expiry time is accurate
+        expires = datetime.datetime.strptime(cert.get_notAfter(),
+                                             '%Y%m%d%H%M%SZ')
+        should_expire = (datetime.datetime.utcnow() +
+                         datetime.timedelta(seconds=2 * 365 * 24 * 60 * 60))
+        diff = should_expire - expires
+        self.assertTrue(diff < datetime.timedelta(seconds=10))
+
+        # Use the openSSL highlevel text output to verify attributes
+        cert_text = crypto.dump_certificate(crypto.FILETYPE_TEXT, cert)
+
+        # Make sure this is a version 3 X509.
+        self.assertIn(six.b("Version: 3"), cert_text)
+
+        # Make sure this cert is marked as Server and Client Cert via the
+        # The extended Key Usage extension
+        self.assertIn(six.b("TLS Web Server Authentication"), cert_text)
+        self.assertIn(six.b("TLS Web Client Authentication"), cert_text)
+
+        # Make sure this cert has the nsCertType server, and client
+        # attributes set
+        self.assertIn(six.b("SSL Server"), cert_text)
+        self.assertIn(six.b("SSL Client"), cert_text)
+
+        # Make sure this cert can't sign other certs
+        self.assertIn(six.b("CA:FALSE"), cert_text)
+
+    def test_generate_private_key(self):
+        bit_length = 1024
+        # Attempt to generate a private key
+        pk = local_cert_gen.LocalCertGenerator._generate_private_key(
+            bit_length=bit_length
+        )
+
+        # Attempt to load the generated private key
+        pko = crypto.load_privatekey(crypto.FILETYPE_PEM, pk)
+
+        # Make sure the bit_length is what we set
+        self.assertEqual(pko.bits(), bit_length)
+
+    def test_generate_private_key_with_passphrase(self):
+        bit_length = 2048
+        # Attempt to generate a private key
+        pk = local_cert_gen.LocalCertGenerator._generate_private_key(
+            bit_length=bit_length,
+            passphrase=self.ca_private_key_passphrase
+        )
+
+        # Attempt to load the generated private key
+        pko = crypto.load_privatekey(crypto.FILETYPE_PEM, pk,
+                                     self.ca_private_key_passphrase)
+
+        # Make sure the bit_length is what we set
+        self.assertEqual(pko.bits(), bit_length)
+
+    def test_generate_csr(self):
+        cn = 'test_cn'
+        # Attempt to generate a CSR
+        csr = local_cert_gen.LocalCertGenerator._generate_csr(
+            cn=cn,
+            private_key=self.ca_private_key,
+            passphrase=self.ca_private_key_passphrase
+        )
+
+        # Attempt to load the generated CSR
+        csro = crypto.load_certificate_request(crypto.FILETYPE_PEM, csr)
+
+        # Make sure the CN is correct
+        self.assertEqual(csro.get_subject().CN, cn)
+
+    def test_generate_cert_key_pair(self):
+        cn = 'test_cn'
+        bit_length = 512
+        # Attempt to generate a cert/key pair
+        cert_object = local_cert_gen.LocalCertGenerator.generate_cert_key_pair(
+            cn=cn,
+            validity=2 * 365 * 24 * 60 * 60,
+            bit_length=bit_length,
+            passphrase=self.ca_private_key_passphrase,
+            ca_cert=self.ca_certificate,
+            ca_key=self.ca_private_key,
+            ca_key_pass=self.ca_private_key_passphrase
+        )
+
+        # Validate that the cert and key are loadable
+        cert = crypto.load_certificate(
+            crypto.FILETYPE_PEM,
+            cert_object.certificate
+        )
+        self.assertIsNotNone(cert)
+
+        key = crypto.load_privatekey(
+            crypto.FILETYPE_PEM,
+            cert_object.private_key,
+            cert_object.private_key_passphrase
+        )
+        self.assertIsNotNone(key)

083ab8cc172a48ab380eaf03050a7b7d9cb80a20
Michael Johnson <johnsomor@gmail.com>
2015-06-18 17:47:41 +0000

    Just a test file - Do not merge
    Change-Id: Ie40476439ab1e56a9fc02e1516417f1cf58b3248

---
+++ octavia/db/api.py
@@ -0,0 +1,36 @@
+#    Copyright 2014 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+from oslo_db.sqlalchemy import session as db_session
+
+_FACADE = None
+
+
+def _create_facade_lazily():
+    global _FACADE
+    if _FACADE is None:
+        _FACADE = db_session.EngineFacade.from_config(cfg.CONF, sqlite_fk=True)
+    return _FACADE
+
+
+def get_engine():
+    facade = _create_facade_lazily()
+    return facade.get_engine()
+
+
+def get_session(expire_on_commit=True):
+    """Helper method to grab session."""
+    facade = _create_facade_lazily()
+    return facade.get_session(expire_on_commit=expire_on_commit)
